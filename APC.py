3f365b044d1bf3be328e59b12b6cb7f0a338e08d
IOConcept <123223096+IOConcept@users.noreply.github.com>
2023-03-07 20:16:23 -0600

    Update APC.py
    updated

--- APC.py
+++ APC.py
@@ -1,19 +1,22 @@
-hourly_rate = float(input("Enter your hourly rate: "))
-overtime_rate = 1.5 * hourly_rate
-incentive_rate = 4.0
+# Get user input
+hourly_rate = float(input("Enter hourly rate: "))
+non_overtime_hours = float(input("Enter number of non-overtime hours worked: "))
+overtime_hours = float(input("Enter number of overtime hours worked: "))
+incentive_percentage = float(input("Enter percentage of eligible hours for incentive pay: "))
+incentive_pay_value = float(input("Enter incentive pay value: "))
 
-non_overtime_hours = float(input("Enter the number of non-overtime hours worked: "))
-overtime_hours = float(input("Enter the number of overtime hours worked: "))
-incentive_percent = float(input("Enter the percentage of hours eligible for incentive pay (e.g. 10 for 10%): "))
+# Calculate regular pay, overtime pay, and incentive pay
+regular_pay = hourly_rate * non_overtime_hours
+overtime_pay = hourly_rate * 1.5 * overtime_hours
+eligible_hours = (non_overtime_hours + overtime_hours) * (incentive_percentage / 100)
+incentive_pay = eligible_hours * incentive_pay_value
 
-total_hours = non_overtime_hours + overtime_hours
-incentive_hours = total_hours * (incentive_percent / 100)
-regular_pay = non_overtime_hours * hourly_rate
-overtime_pay = overtime_hours * overtime_rate
-incentive_pay = incentive_hours * incentive_rate
+# Calculate total pay
 total_pay = regular_pay + overtime_pay + incentive_pay
 
-print("Regular pay for non-overtime hours: ${:.2f}".format(regular_pay))
-print("Overtime pay for overtime hours: ${:.2f}".format(overtime_pay))
-print("Incentive pay for eligible hours: ${:.2f}".format(incentive_pay))
-print("Total pay: ${:.2f}".format(total_pay))
+# Print results
+print(f"Regular pay: ${regular_pay:.2f}")
+print(f"Overtime pay: ${overtime_pay:.2f}")
+print(f"Incentive pay: ${incentive_pay:.2f}")
+print(f"Total pay: ${total_pay:.2f}")
+
