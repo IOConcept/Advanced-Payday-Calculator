04505291697704c7aa3343629649f935c0939465
IOConcept <123223096+IOConcept@users.noreply.github.com>
2023-03-07 21:07:03 -0600

    Update APC.py
    here's a summary of the updates made to the first draft of the program:
    
    Added user input prompts for hourly rate, non-overtime hours worked, overtime hours worked, percentage of eligible hours for incentive pay, and incentive pay value.
    Added error checking to ensure that only numerical values are entered for user inputs.
    Updated the calculation of incentive pay to allow for a user-defined incentive pay value.
    Added comments to the code for better readability and organization.
    Updated the README file to provide a brief overview of the program.

--- APC.py
+++ APC.py
@@ -1,15 +1,46 @@
 # Get user input
-hourly_rate = float(input("Enter hourly rate: "))
-non_overtime_hours = float(input("Enter number of non-overtime hours worked: "))
-overtime_hours = float(input("Enter number of overtime hours worked: "))
-incentive_percentage = float(input("Enter percentage of eligible hours for incentive pay: "))
-incentive_pay_value = float(input("Enter incentive pay value: "))
+while True:
+    try:
+        hourly_rate = float(input("Enter hourly rate: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a number for hourly rate.")
+
+while True:
+    try:
+        non_overtime_hours = float(input("Enter number of non-overtime hours worked: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a number for non-overtime hours worked.")
+
+while True:
+    try:
+        overtime_hours = float(input("Enter number of overtime hours worked: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a number for overtime hours worked.")
+
+while True:
+    try:
+        incentive_percentage = float(input("Enter percentage of eligible hours for incentive pay: "))
+        if incentive_percentage < 0 or incentive_percentage > 100:
+            raise ValueError
+        break
+    except ValueError:
+        print("Invalid input. Please enter a number between 0 and 100 for the incentive percentage.")
+
+while True:
+    try:
+        incentive_pay = float(input("Enter incentive pay value: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a number for incentive pay value.")
 
 # Calculate regular pay, overtime pay, and incentive pay
 regular_pay = hourly_rate * non_overtime_hours
 overtime_pay = hourly_rate * 1.5 * overtime_hours
 eligible_hours = (non_overtime_hours + overtime_hours) * (incentive_percentage / 100)
-incentive_pay = eligible_hours * incentive_pay_value
+incentive_pay = eligible_hours * incentive_pay
 
 # Calculate total pay
 total_pay = regular_pay + overtime_pay + incentive_pay
